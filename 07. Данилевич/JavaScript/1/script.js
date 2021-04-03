9e718b2ce8e6dad347ca40cc9e44bb3745ebe2fe
pasha-danilevich <pasha-danilevich@outlook.com>
2021-04-03 11:03:24 +0300

    жава

---
+++ 07. Данилевич/JavaScript/1/script.js
@@ -0,0 +1,22 @@
+// 1
+// var  Tc = 10
+// var Tf = (9 / 5) * Tc + 3
+
+// alert(Tc + " градусов по Целсию ровно " 
+// 	+ Tf + " градусов по Фаренгейту ")
+
+
+
+// 2
+// var name = "Vasiliy"
+// var admin = name
+
+// alert(admin)
+
+
+// 3
+// var www = 1000 + "108"
+// alert(www)
+
+
+// 4
